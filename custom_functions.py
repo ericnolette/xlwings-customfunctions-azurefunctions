886783f71b30fa74ab42e75f6a11551c41c2e18b
Eric Nolette <ericnolette@Erics-MBP-2.fios-router.home>
2024-03-30 20:28:48 -0400

    u

--- custom_functions.py
+++ custom_functions.py
@@ -103,19 +103,19 @@ def timeseries_start(df):
 def last_calculated():
     return f"Last calculated: {dt.datetime.now()}"
 
-@pro.func
-async def layoffs_fyi():
-    sql = '''  
-    SELECT 
-    date(date) as date,
-    company,
-    employees_laid_off,
-    concat(cast(round(percent_laid_off*100,2) as string),"%") as percent_laid_off,
-    datamachine_load_time
-    FROM `datamachine-407200.macro.layoffs_fyi`
-    WHERE datamachine_load_time = (select max(datamachine_load_time) from `datamachine-407200.macro.layoffs_fyi`)
-    ORDER BY date desc
-    '''
-    async with bigquery.Client(credentials=credentials) as client:
-        df = client.query(sql).to_dataframe()
-    return df
+# @pro.func
+# async def layoffs_fyi():
+#     sql = '''  
+#     SELECT 
+#     date(date) as date,
+#     company,
+#     employees_laid_off,
+#     concat(cast(round(percent_laid_off*100,2) as string),"%") as percent_laid_off,
+#     datamachine_load_time
+#     FROM `datamachine-407200.macro.layoffs_fyi`
+#     WHERE datamachine_load_time = (select max(datamachine_load_time) from `datamachine-407200.macro.layoffs_fyi`)
+#     ORDER BY date desc
+#     '''
+#     async with bigquery.Client(credentials=credentials) as client:
+#         df = client.query(sql).to_dataframe()
+#     return df
